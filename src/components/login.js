571ad406a8203782d3a54870c004d80af5fe7da7
K-s-c49 <khushalsinghchundawat144@gmail.com>
2025-09-04 17:39:47 +0530

    Updated project

--- src/components/login.js
+++ src/components/login.js
@@ -19,7 +19,7 @@ const Login = () => {
     setError("");
 
     try {
-      const res = await axios.post("http://localhost:5000/api/login", formData);
+      const res = await axios.post("https://backend-626x.onrender.com/api/login", formData);
       const { token, user } = res.data;
 
       localStorage.setItem("token", token);
